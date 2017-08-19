78ef04ae52f7502edf6ca3f2f8775d2597565b06
Rhut Vasavada <rhutvasavada@gmail.com>
2017-08-19 13:13:34 -0400

    put logout button in a card section

--- src/app.js
+++ src/app.js
@@ -5,52 +5,52 @@ import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  state = { loggedIn: null };
+    state = { loggedIn: null };
 
-  componentWillMount() {
-    firebase.initializeApp({
-        apiKey: "AIzaSyAEve0as30hObWFbue5QSlH4CoV5mAfP5E",
-        authDomain: "auth-9f93c.firebaseapp.com",
-        databaseURL: "https://auth-9f93c.firebaseio.com",
-        projectId: "auth-9f93c",
-        storageBucket: "auth-9f93c.appspot.com",
-        messagingSenderId: "907819360793"
-    });
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAEve0as30hObWFbue5QSlH4CoV5mAfP5E",
+            authDomain: "auth-9f93c.firebaseapp.com",
+            databaseURL: "https://auth-9f93c.firebaseio.com",
+            projectId: "auth-9f93c",
+            storageBucket: "auth-9f93c.appspot.com",
+            messagingSenderId: "907819360793"
+        });
 
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ loggedIn: true });
-      } else {
-        this.setState({ loggedIn: false });
-      }
-    });
-  }
-
-  renderContent() {
-    switch (this.state.loggedIn) {
-      case true:
-        return (
-          <CardSection>
-              <Button onPress={() => firebase.auth().signOut()}>
-                Log Out
-            </Button>
-        </CardSection>
-        );
-      case false:
-        return <LoginForm />;
-      default:
-        return <Spinner size="large" />;
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
     }
-  }
 
-  render() {
-    return (
-      <View>
-        <Header headerText="Authentication" />
-            {this.renderContent()}
-      </View>
-    );
-  }
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+            </Button>
+                    </CardSection>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large" />;
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Header headerText="Authentication" />
+                {this.renderContent()}
+            </View>
+        );
+    }
 }
 
 export default App;
